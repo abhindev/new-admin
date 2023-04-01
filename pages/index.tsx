5315a84b9c513fbae9a51718f4fcf30fbad3241f
Abhin dev <abhindev4432@gmail.com>
2023-04-01 20:03:31 +0530

    a

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,7 @@ export default function Home({
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>admin</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
